b4ef19e6135373421f8b4967b23c622063e8e5ab
Artiom Duca <duca.artiom@gmail.com>
2020-11-13 17:25:43 +0200

    configure apollo hello world

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,13 @@
 import expressLoader from './express';
 import mongooseLoader from './mongoose';
+import apolloLoader from './apollo';
 import telegramBot from './telegramBot';
 import Logger from './logger';
 import config from '../config';
 
 export default async ({ expressApp }) => {
+  await apolloLoader({ app: expressApp });
+  Logger.info('Apollo Loaded');
   await expressLoader({ app: expressApp });
   Logger.info('Express Loaded');
   await mongooseLoader(config.databaseURL);
